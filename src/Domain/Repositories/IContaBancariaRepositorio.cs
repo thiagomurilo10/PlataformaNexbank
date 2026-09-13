48264e3276636878d90ba487daf595b3699fc280
Thiago Murilo <thiagomayta10@gmail.com>
2026-09-13 03:30:03 -0300

    feat(api): adiciona endpoints de criação e consulta de conta

---
+++ src/Domain/Repositories/IContaBancariaRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using PlataformaNexbank.Domain.Entities;
+
+namespace PlataformaNexbank.Domain.Repositories;
+
+public interface IContaBancariaRepositorio
+{
+    void Adicionar(ContaBancaria conta);
+    ContaBancaria? ObterPorId(Guid id);
+}
