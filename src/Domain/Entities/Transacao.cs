05bc4300b95a0dc965fc7b633d51a22807b4bc95
Thiago Murilo <thiagomayta10@gmail.com>
2026-09-15 20:20:27 -0300

    feat(conta): adiciona entidade Transacao e exceção SaldoInsuficienteException"

---
+++ src/Domain/Entities/Transacao.cs
@@ -0,0 +1,21 @@
+﻿using PlataformaNexbank.Domain.Enums;
+
+namespace PlataformaNexbank.Domain.Entities;
+
+// Representa uma movimentação financeira (depósito ou saque) já ocorrida.
+// É imutável: uma vez criada, seus dados não podem ser alterados, pois representa um histórico
+public class Transacao
+{
+    public Guid Id { get; private set; }         // Identificador único da transação
+    public TipoTransacao Tipo { get; private set; }     // Se foi depósito ou saque
+    public decimal Valor { get; private set; }      // Valor movimentado (sempre positivo)
+    public DateTime DataHora { get; private set; }      // Registra o momento q foi feito a transacao
+
+    public Transacao(TipoTransacao tipo, decimal valor)
+    {
+        Id = Guid.NewGuid();
+        Tipo = tipo;
+        Valor = valor;
+        DataHora = DateTime.UtcNow;
+    }
+}
---
+++ src/Domain/Enums/TipoTransacao.cs
@@ -0,0 +1,8 @@
+﻿namespace PlataformaNexbank.Domain.Enums;
+
+// Representa os tipos possíveis de movimentação financeira em uma conta.
+public enum TipoTransacao
+{
+    Deposito, // Entrada de valor na conta
+    Saque     // Saída de valor da conta
+}
